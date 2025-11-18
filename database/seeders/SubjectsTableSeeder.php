1e0f373153bdf26566be36359d78b7613b2eaacc
nishikawa shiho <>
2025-11-18 11:29:14 +0900

    ・初期データの登録 ・デバッグバーの導入 ・ユーザーの新規登録画面にバリデーションを追加 ・usersテーブルとsubjectsテーブルのリレーションを定義 ・サブカテゴリーの追加 ・投稿の編集/削除
    上記完了済みです。ご確認お願いいたします。

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        // 国語、数学、英語を追加
+        $subjects = [
+            ['subject' => '国語'],
+            ['subject' => '数学'],
+            ['subject' => '英語'],
+        ];
+
+        DB::table('subjects')->insert($subjects);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +16,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $users = [
+            [
+                'over_name' => '西川', 'under_name' => '詩歩',
+                'over_name_kana' => 'ニシカワ', 'under_name_kana' => 'シホ',
+                'mail_address' => 'shiho@gmail.com',
+                'sex' => 2,
+                'birth_day' => Carbon::create(1997, 10, 22),
+                'role' => 4,
+                'password' => Hash::make('password'),
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'over_name' => '星野', 'under_name' => '源',
+                'over_name_kana' => 'ホシノ', 'under_name_kana' => 'ゲン',
+                'mail_address' => 'gen@gmail.com',
+                'sex' => 1,
+                'birth_day' => Carbon::create(1991, 1, 28),
+                'role' => 1,
+                'password' => Hash::make('password'),
+                'created_at' => Carbon::now(),
+            ],
+        ];
+        DB::table('users')->insert($users);
 
     }
 }
